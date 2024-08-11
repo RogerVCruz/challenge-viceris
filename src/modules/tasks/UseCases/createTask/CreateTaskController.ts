4ad5f2ff386b0bb61a922c6f86890bd8e331fb8e
Roger Vinicius <rogervtsc@hotmail.com>
2024-08-11 14:47:56 -0300

    chore: set status to lower case

--- src/modules/tasks/UseCases/createTask/CreateTaskController.ts
+++ src/modules/tasks/UseCases/createTask/CreateTaskController.ts
@@ -2,14 +2,15 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import { CreateTaskUseCase } from './CreateTaskUseCase';
-import { priorityOptions } from '../../entities/Task';
-import { AppError } from '../../../../shared/errors/AppError';
+
 
 class CreateTaskController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { description, priority } = request.body;
+    let { description, priority } = request.body;
 
     const user_id = request.user.id;
+
+    priority = priority.toLowerCase()
 
     const createTaskUseCase = container.resolve(CreateTaskUseCase);
 
--- src/modules/tasks/UseCases/setStatus/SetStatusController.ts
+++ src/modules/tasks/UseCases/setStatus/SetStatusController.ts
@@ -6,7 +6,7 @@ import { TaskStatus } from '../../entities/Task';
 class SetStatusController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
-    const status: TaskStatus = request.body.status;
+    const status: TaskStatus = request.body.status.toLowerCase();
 
     const setStatusUseCase = container.resolve(SetStatusUseCase);
 
