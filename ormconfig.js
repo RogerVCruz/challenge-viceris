8b8357a848e69d5efb67995356567cc480a9ea77
Roger Vinicius <rogervtsc@hotmail.com>
2024-08-11 14:46:50 -0300

    chore: ormconfig for prod and dev

--- ormconfig.js
+++ ormconfig.js
@@ -1,18 +1,26 @@
-const path = require("path");
+const path = require('path');
 
 const paths = {
   entities: path.resolve(__dirname, './src/modules/**/entities/*.ts'),
-  migrations: path.resolve(__dirname, 'src/database/migrations/*.ts')
-}
+  migrations: path.resolve(__dirname, 'src/database/migrations/*.ts'),
+};
 
-if (!process[Symbol.for("ts-node.register.instance")]) {
-  paths.entities = path.resolve(__dirname, 'dist', './src/modules/**/entities/*{.ts,.js}')
-  paths.migrations = path.resolve(__dirname, 'dist', 'src/database/migrations/*{.ts,.js}')
+if (process.env.NODE_ENV !== 'development') {
+  paths.entities = path.resolve(
+    __dirname,
+    'dist',
+    './src/modules/**/entities/*{.ts,.js}',
+  );
+  paths.migrations = path.resolve(
+    __dirname,
+    'dist',
+    'src/database/migrations/*{.ts,.js}',
+  );
 }
 
 module.exports = {
   type: 'sqlite',
-  database: 'src/db.sqlite',
+  database: ':memory:',
   synchronize: true,
   logging: false,
   entities: [paths.entities],
